f1ca817ee4e80259723c159ab27b68e0f5d4d37a
Shubham Saurav <shubhamsinha186@gmail.com>
2020-08-21 20:37:22 +0530

    Changed port to second port to 500

--- server.js
+++ server.js
@@ -5,16 +5,20 @@ const dotenv = require('dotenv');
 
 const app = express();
 
+const tourRoute = require('./routes/tours');
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan());
 }
 
-app.get('/', (req, res, next) => {
-  res.status(200).json({ success: true, data: 'All working!' });
+app.use('/api/v1/tours', tourRoute);
+
+app.all('*', (req, res, next) => {
+  res.status(200).json({ success: false, message: 'Route Not Found' });
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log('Listening to Server on : ', PORT);
+  console.log(`Listening to server on port: ${PORT}`.cyan.underline);
 });
